4055ed1b8d9a6bcd9c6b5fd23035e5ad55d7b2c8
WuHoUnited <iforgotmypassphrase@gmail.com>
2013-01-15 22:37:50 -0500

    First Commit

---
+++ crossword/project.clj
@@ -0,0 +1,7 @@
+(defproject crossword "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+					[org.clojure/core.logic "0.7.5"]]
+  :plugins [[lein-marginalia "0.7.1"]]
+  :repl-options {:port 9000}
+  )
