e5b2eabdec2ee264acd5dcb60ffc463ccb04f8c2
WuHoUnited <iforgotmypassphrase@gmail.com>
2014-09-28 01:00:51 -0400

    Update project.clj documentation.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject crossword "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "Logic programming implementation of a word-search type algorithm."
+  :url "https://github.com/WuHoUnited/logic-word-search"
   :dependencies [[org.clojure/clojure "1.3.0"]
 					[org.clojure/core.logic "0.7.5"]]
   :plugins [[lein-marginalia "0.7.1"]]
